25b742b728102ad46e1bfabce888c7c251238bc9
Enrique Ibarra <rique.ibarra1@gmail.com>
2020-06-09 17:17:12 -0500

    changed some components to use reducer instead of state

--- src/screens/CounterScreen.js
+++ src/screens/CounterScreen.js
@@ -1,17 +1,34 @@
-import React, {useState} from 'react';
+import React, {useReducer} from 'react';
 import {Text, StyleSheet, View, Button } from 'react-native';
 
+const INCREMENT = 1;
+//action is how to change state.
+const reducer = (state, action) => {
+    //state === {count: number} 
+    //action == {type: 'increase' || 'deacrease', payload: }
+    switch(action.type) {
+        case 'increase' : 
+        //you cannot change state directly, return a new object with the old state rewritten for the function to do its thing. just do it.
+            return {...state, count: state.count + action.payload};
+        case 'decrease' : 
+            return {...state, count: state.count + action.payload};
+        default:
+            return state;
+    }
+}
 const CounterScreen = () => {
-    const [counter, setCounter] = useState(0);
+    //The second requirement for useReducer must/should be an object
+    const [state, dispatch] = useReducer( reducer, {
+        count: 0
+    });
+    const {count} = state;
     return(
         <View>
-            <Button title="Increase" onPress={() => {
-                setCounter(counter + 1);
-            }}/>
-            <Button title="Decrease" onPress={() => {
-                setCounter(counter - 1);
-            }}/>
-            <Text>Current Count: {counter} </Text>
+            <Button title="Increase" onPress={() => dispatch({type: 'increase', payload: INCREMENT})}
+            />
+            <Button title="Decrease" onPress={() => dispatch(   {type: 'decrease', payload: -1 * INCREMENT })}
+            />
+            <Text>Current Count: {count} </Text>
         </View>
     );
 };
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -11,6 +11,7 @@ const HomeScreen = ({navigation}) => {
           <Button title="Go to Images Demo" onPress={() => navigation.navigate('Images')} />
           <Button title="Go to Counter State Demo" onPress={() => navigation.navigate('Counter')} />
           <Button title="Go to Color Demo" onPress={() => navigation.navigate('Color')} />
+          <Button title="Go to Change Color Demo" onPress={() => navigation.navigate('ChangeColor')} />
     </View>
   );
 };
